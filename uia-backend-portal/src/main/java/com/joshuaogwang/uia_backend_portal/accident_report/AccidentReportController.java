7e28bf81b9712ec0dc5a370a94cf6d599a35e684
Joshua Ogwang <joshua.ogwang@hotmail.com>
2024-09-23 15:24:22 +0300

    ADD: Fetch all accident reports for admin

--- uia-backend-portal/src/main/java/com/joshuaogwang/uia_backend_portal/accident_report/AccidentReportController.java
+++ uia-backend-portal/src/main/java/com/joshuaogwang/uia_backend_portal/accident_report/AccidentReportController.java
@@ -2,10 +2,9 @@ package com.joshuaogwang.uia_backend_portal.accident_report;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/accident-reports")
@@ -16,5 +15,10 @@ public class AccidentReportController {
     @PostMapping
     public ResponseEntity<AccidentReportResponse> submitAccidentReport(@RequestBody AccidentReportRequest accidentReportRequest) {
         return ResponseEntity.ok(accidentReportService.submitAccidentReport(accidentReportRequest));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AccidentReport>> getAccidentReports() throws Exception {
+        return ResponseEntity.ok(accidentReportService.getAccidentReports());
     }
 }
--- uia-backend-portal/src/main/java/com/joshuaogwang/uia_backend_portal/accident_report/AccidentReportService.java
+++ uia-backend-portal/src/main/java/com/joshuaogwang/uia_backend_portal/accident_report/AccidentReportService.java
@@ -1,14 +1,20 @@
 package com.joshuaogwang.uia_backend_portal.accident_report;
 
+import com.joshuaogwang.uia_backend_portal.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.Date;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AccidentReportService {
     private final AccidentReportRepository accidentReportRepository;
+    private final UserRepository userRepository;
 
     public AccidentReportResponse submitAccidentReport(AccidentReportRequest accidentReportRequest) {
         var accidentReport = AccidentReport
@@ -37,5 +43,13 @@ public class AccidentReportService {
                 .message("Accident Report submitted successfully.")
                 .accidentReport(submittedAccidentReport)
                 .build();
+    }
+
+    public List<AccidentReport> getAccidentReports() throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.getAuthorities().stream().noneMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ADMIN"))){
+            throw new Exception("Access Denied");
+        }
+        return accidentReportRepository.findAll();
     }
 }
