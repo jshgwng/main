23edcb7981bf1ec693dc371b0e4e83252ae983ba
Joshua Ogwang <joshua.ogwang@hotmail.com>
2024-09-24 16:44:42 +0300

    FIX: Remove autowired service

--- uia-backend-portal/src/main/java/com/joshuaogwang/uia_backend_portal/notification/NotificationService.java
+++ uia-backend-portal/src/main/java/com/joshuaogwang/uia_backend_portal/notification/NotificationService.java
@@ -1,14 +1,15 @@
 package com.joshuaogwang.uia_backend_portal.notification;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class NotificationService {
-    @Autowired
-    private JavaMailSender javaMailSender;
+    private final JavaMailSender javaMailSender;
 
     public void sendEmailAlert(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
