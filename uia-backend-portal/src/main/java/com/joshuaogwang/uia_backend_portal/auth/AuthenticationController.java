c14e5f21e3b1b7e97da5773b5698b836da1a2e0b
Joshua Ogwang <joshua.ogwang@hotmail.com>
2024-09-24 09:58:16 +0300

    ADD: Untrack config file

--- uia-backend-portal/src/main/java/com/joshuaogwang/uia_backend_portal/auth/AuthenticationController.java
+++ uia-backend-portal/src/main/java/com/joshuaogwang/uia_backend_portal/auth/AuthenticationController.java
@@ -1,22 +1,33 @@
 package com.joshuaogwang.uia_backend_portal.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
-@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
+
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
-        return ResponseEntity.ok(authenticationService.register(request));
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        try {
+            AuthenticationResponse authenticationResponse = authenticationService.register(request);
+            return ResponseEntity.ok(authenticationResponse);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody LoginRequest request) {
-        return ResponseEntity.ok(authenticationService.login(request));
+    public ResponseEntity<?> register(@RequestBody LoginRequest request) {
+        try {
+            AuthenticationResponse authenticationResponse = authenticationService.login(request);
+            return ResponseEntity.ok(authenticationResponse);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
     }
 }
