7eee899a11f4f68c55ef81768dd4ceb27cf2d08e
Joshua Ogwang <joshua.ogwang@hotmail.com>
2024-09-24 10:42:21 +0300

    ADD: User Repository

---
+++ uia-backend-portal/src/main/java/com/joshuaogwang/uia_backend_portal/user/profile/UserProfileRepository.java
@@ -0,0 +1,6 @@
+package com.joshuaogwang.uia_backend_portal.user.profile;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserProfileRepository extends JpaRepository<UserProfile,Integer> {
+}
