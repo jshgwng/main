011438bf874fc935e76b58aa98bc298bf9f12f85
Joshua Ogwang <joshua.ogwang@hotmail.com>
2024-09-24 10:30:56 +0300

    ADD: User profile

---
+++ uia-backend-portal/src/main/java/com/joshuaogwang/uia_backend_portal/user/profile/UserProfile.java
@@ -0,0 +1,39 @@
+package com.joshuaogwang.uia_backend_portal.user.profile;
+
+import com.joshuaogwang.uia_backend_portal.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_profile")
+public class UserProfile {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private Date dateOfBirth;
+    private String languagePreference;
+    private String theme;
+    private String notificationSettings;
+    private String socialLinks;
+    private String profilePictureUrl;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String country;
+    private String postalCode;
+    @Lob
+    private String bio;
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+}
